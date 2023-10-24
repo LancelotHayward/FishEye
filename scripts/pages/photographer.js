98d721d9b9eb85f325947103bc955aaea8b05968
LancelotHayward <hayward.lance@gmail.com>
2023-10-24 17:03:24 +0200

    Contact form

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,3 +2,12 @@
 function toggleModal() {
 	document.getElementById("contact_modal").classList.toggle("display-none")
 }
+function submitModal() {
+    const message_firstname = document.getElementById("message_firstname").value
+    const message_lastname = document.getElementById("message_lastname").value
+    const message_email = document.getElementById("message_email").value
+    const message_content = document.getElementById("message_content").value
+    const message = message_firstname + " " + message_lastname + " (" + message_email + ") sent " + message_content
+    console.log(message)
+    toggleModal()
+}
